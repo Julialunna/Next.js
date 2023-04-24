56fce2fd9e98f0ade4496c5f5dd2e4d2fd25d108
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-04-24 17:33:35 -0300

    começando css

--- alura-cases-divulgacao/src/componentes/Link/index.js
+++ alura-cases-divulgacao/src/componentes/Link/index.js
@@ -1,4 +1,11 @@
-import NextLink from 'next/link'
+import NextLink from 'next/link';
+function Tittle(){
+    return(
+        <h1>
+            {children}
+        </h1>
+    )
+}
 export default function Link({children, href, ...props}){
     return(
     <NextLink href={href}>
