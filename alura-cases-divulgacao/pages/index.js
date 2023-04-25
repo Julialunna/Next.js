4c8daeac8ef69ae1f3759603906d0c8335a00c6e
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-04-25 14:56:13 -0300

    usando estilizações prontas e colocando imagens

--- alura-cases-divulgacao/pages/index.js
+++ alura-cases-divulgacao/pages/index.js
@@ -1,12 +1,37 @@
+import HomeScreen from "../src/screens/HomeScreen";
+export default HomeScreen;
+
+/*import React from 'react';
 import Link from '../src/componentes/Link/';
+import Footer from '../src/componentes/patterns/Footer';
+
+
+function Tittle({children, as}){
+    const Tag = as;
+    return(
+        <React.Fragment>
+        <Tag>
+            {children}
+        </Tag>
+        <style jsx>{`
+            ${Tag}{
+                color: red;
+            }
+        `
+        }
+        </style>
+        </React.Fragment>
+    )
+}
 
 export default function homePage(){
     return(
         <div>
-            <h1>Alura Cases - Home</h1>
+            <Tittle as='h2'>Alura Cases - Home</Tittle>
             <Link href="/faq">
             Ir para a página do FAQ
             </Link>
+            <Footer/>
         </div>
     )
-}
+}*/
--- alura-cases-divulgacao/src/componentes/Link/index.js
+++ alura-cases-divulgacao/src/componentes/Link/index.js
@@ -1,11 +1,4 @@
 import NextLink from 'next/link';
-function Tittle(){
-    return(
-        <h1>
-            {children}
-        </h1>
-    )
-}
 export default function Link({children, href, ...props}){
     return(
     <NextLink href={href}>
