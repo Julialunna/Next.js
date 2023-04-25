98176158f33e6d5b597c2123c4e1af5b8a6f6f40
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-04-25 15:51:36 -0300

    aprendendo diferença static props e server side props

--- alura-cases-divulgacao/pages/faq.js
+++ alura-cases-divulgacao/pages/faq.js
@@ -1,12 +1,47 @@
+import { useEffect, useState } from 'react';
 import Link from '../src/componentes/Link/';
+//em modo dev sempre roda, em acesso não
+export async function getStaticProps(context){
 
-export default function FAQPage(){
+    const FAQ_API_URL='https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json '
+    const faq=await fetch(FAQ_API_URL)
+    .then((repostaDoServidor)=>{
+        return repostaDoServidor.json();
+    })
+    .then((resposta)=>{
+        return resposta
+    })
+    return{
+        props:{faq}
+    };
+}
+
+export default function FAQPage({faq}){
+
+    /*const [faq, setFaq]=useState([])
+    useEffect(()=>{
+    
+    }, [])*/
+
     return(
         <div>
             <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
             <Link href="/">
             Ir para a home
             </Link>
+            <ul>
+                {faq.map(({answer, question})=>{
+                    return(
+
+                    <li key={question}>
+                        <article>
+                            <h2>{question}</h2>
+                            <p>{answer}</p>
+                        </article>
+                    </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
