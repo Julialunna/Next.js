5f0bdb7b3a54eb5b6afa790863554c553e48d2fa
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-05-02 15:55:11 -0300

    começando a fazer login

--- next-3-jwt-projeto_inicial/frontend/pages/index.js
+++ next-3-jwt-projeto_inicial/frontend/pages/index.js
@@ -1,16 +1,54 @@
+import React from "react";
+import {useRouter} from "next/router"
+import { authService } from "../src/services/auth/auth-service.js";
+
 export default function HomeScreen() {
+  const router = useRouter();
+  const [values, setValues]=React.useState({
+    usuario:"omariosouto", 
+    senha:"safepassword"
+  });
+
+  function handleChange(event){
+    const fieldValue = event.target.value;
+    const fieldName = event.target.name;
+    setValues((currentValues)=>{
+      return{
+        ...currentValues, 
+        [fieldName]:fieldValue,
+      };
+    })
+  }
+
   return (
     <div>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={(event)=>{
+        event.preventDefault();
+        authService
+        .login({
+          username:values.usuario,
+          password:values.senha,
+        })
+        .then(()=>{
+          router.push('/auth-page-ssr');
+          //router.push('/auth-page-static');
+        })
+        .catch(()=>{
+          alert("Usuário ou a senha estão inválidos")
+        })
+      }}>
         <input
           placeholder="Usuário" name="usuario"
-          defaultValue="omariosouto"
+          value={values.usuario} onChange={handleChange}
         />
         <input
           placeholder="Senha" name="senha" type="password"
-          defaultValue="safepassword"
+          value={values.senha} onChange={handleChange}
         />
+        {/*<pre>
+          {JSON.stringify(values, null, 2)}
+        </pre>*/}
         <div>
           <button>
             Entrar
